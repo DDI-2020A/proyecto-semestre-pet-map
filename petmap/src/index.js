78c640992f8e28e99dcdd1901d807b5f4195408c
DESKTOP-SMED30I\cocho <elvis.agila@epn.edu.ec>
2020-09-01 16:32:42 -0500

    Paso de menu como componente

--- petmap/src/index.js
+++ petmap/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import Mision from "./components/Mision";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Mision />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
