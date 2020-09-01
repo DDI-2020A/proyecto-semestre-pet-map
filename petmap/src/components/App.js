ac9c428f1da8e7a54e9db45bdc79cac359ec8790
DESKTOP-SMED30I\cocho <elvis.agila@epn.edu.ec>
2020-08-31 20:45:28 -0500

    Encapsulamiento del menu

--- petmap/src/components/App.js
+++ petmap/src/components/App.js
@@ -1,72 +1,59 @@
 import React, {useState, useEffect} from 'react';
 
 import '../styles/App.css';
-import {Layout, Menu, Row, Col, Button,Typography} from 'antd';
+import {Layout, Row, Col, Button,Typography} from 'antd';
+import { BrowserRouter as Router } from 'react-router-dom';
 import logo from '../images/Logo.PNG';
 import img1 from '../images/inicio.jpeg';
-import {HomeOutlined,FormOutlined,NotificationOutlined,ApartmentOutlined,SmileOutlined,QuestionCircleOutlined} from '@ant-design/icons';
+import Navigation from "./Navigation";
+
 
 const { Header, Footer, Content } = Layout;
 const {Title} =Typography;
 
+
 const App = () => {
-
-
-    const [current, setCurrent] = useState('home');
 
     return (
 
-        <Layout className='layout'>
-          <Header className='main-header'>
-              <div className='logo'>
-                  <img src={ logo } alt='PET MAP' />
-              </div>
-              <Menu className='main-menu'
-                    onClick={(e)=>setCurrent(e.key)}
-                    selectedKeys={[current]}
-                     mode='horizontal'>
-                  <Menu.Item key='home' icon={<HomeOutlined />}>
-                      Home
-                  </Menu.Item>
-                  <Menu.Item key='registro' icon={<FormOutlined />}>
-                      Registro Adopción
-                  </Menu.Item>
-                  <Menu.Item key='noticias' icon={<NotificationOutlined />}>
-                      Noticias
-                  </Menu.Item>
-                  <Menu.Item key='servicios' icon={<ApartmentOutlined />}>
-                      Servicios
-                  </Menu.Item>
-                  <Menu.Item key='derechos' icon={<SmileOutlined />}>
-                      Derechos Animales
-                  </Menu.Item>
-                  <Menu.Item key='about' icon={<QuestionCircleOutlined />}>
-                      ¿Quienes Somos?
-                  </Menu.Item>
-              </Menu>
-          </Header>
-          <Content className='main-content'>
-            <Row>
-                <Col span = {15}>
-                    <div className='imgPp'>
-                        <img src={img1} alt="PerroGato"/>
-                    </div>
-                </Col>
-                <Col span = {9}>
-                    <div className='box'>
-                        <Title>PET <br/> MAP</Title>
-                        <Title level = {2} className='title2 mb-5 mt-5'>Tu acción puede cambiar vidas</Title><br/><br/><br/><br/>
-                        <Button type="primary">REGISTRARSE</Button><br/> <br/>
-                        <Button type="primary">INICIAR SESIÓN</Button>
-                    </div>
-                </Col>
-            </Row>
-          </Content>
-          <Footer className= 'main-footer'>
-              <div >Pet Map - Derechos reservados 2020</div>
-          </Footer>
+            <Layout className='layout'>
+              <Header className='main-header'>
+                  <div className='logo'>
+                      <img src={ logo } alt='PET MAP' />
+                  </div>
 
-        </Layout>
+                  <Navigation />
+              </Header>
+              <Content className='main-content mb-3'>
+                <Row>
+                    <Col span = {15}>
+                        <div className='imgPp'>
+                            <img src={img1} alt="PerroGato"/>
+                        </div>
+                    </Col>
+                    <Col span = {9}>
+                        <div className='box'>
+                            <div className='boxCenter'>
+                                <Title className='title1'>PET  MAP</Title>
+                                <Title level = {2} className='title2 '>TU ACCIÓN PUEDE CAMBIAR VIDAS</Title>
+
+                                <Button type="primary" shape="round" >
+                                    REGISTRARSE
+                                 </Button><br/><br/>
+                                <Button type="primary" shape="round" >
+                                    INICIAR SESIÓN
+                                </Button>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+              </Content>
+              <Footer className= 'main-footer'>
+                  <div ><Title level = {3} > Pet Map - Derechos reservados 2020 </Title></div>
+              </Footer>
+
+            </Layout>
+
         );
 };
 
---
+++ petmap/src/pages/registro.js
@@ -0,0 +1,12 @@
+
+import React, { useState } from 'react';
+
+
+const Registro =()=>{
+    return(<>
+        REGISTRO
+    </>
+    );
+}
+
+export default Registro;
