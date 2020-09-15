71e985bf1cc486a06d1fe1b0b8fe66d7e0d2f764
CarlosMontesdeoca <66446182+CarlosMontesdeoca@users.noreply.github.com>
2020-09-15 10:04:58 -0500

    cambio de sdseño de form

--- petmap/src/routers/LinksPages.js
+++ petmap/src/routers/LinksPages.js
@@ -9,7 +9,7 @@ import Donacionesfisicas from "../pages/Donacionesfisicas";
 import Donacionestransferencia from "../pages/Donacionestransferencia";
 import Donacionesdebito from "../pages/Donacionesdebito";
 import Derechos from "../pages/Derechos";
-import Regpet from "../pages/Regpet";
+import RegisterPet from "../pages/RegisterPet";
 import Galeria from "../pages/Galeria";
 import Mision from "../pages/Mision";
 import Reg_Adop from "../pages/First_adop";
@@ -52,7 +52,7 @@ const AppRouter = () => {
                 <Adopciones />
         </Route>
         <Route path={ Routes.REGISTROANIMAL }>
-               <Regpet />
+               <RegisterPet />
         </Route>
             <Route path={ Routes.ADOPTA }>
                 <Galeria />
