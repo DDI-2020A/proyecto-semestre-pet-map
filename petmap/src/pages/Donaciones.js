04d573996cc3c3f7fe458947066432b065e1946b
yomara0604 <yomara.diaz@ep.edu.ec>
2020-09-21 12:40:29 -0500

    Interface home

--- petmap/src/pages/Donaciones.js
+++ petmap/src/pages/Donaciones.js
@@ -1,36 +1,37 @@
 import React from "react";
-import { Card, Col, Row} from 'antd';
+import { Col, Row} from 'antd';
+import "../styles/donaciones.less";
 import img1 from "../images/perrodonaciones.svg";
 import img2 from "../images/gatodonaciones.svg";
 import { Button } from 'antd';
 
 import Routes from "../constants/routes";
 import {Link} from 'react-router-dom';
-import "../styles/donaciones.less";
-
-
 
 
 
 const Donaciones = () => (
 
-    <div className="site-card-wrapper">
-        <Row gutter={16}>
-            <Col span={8}>
-                    <img src={img1} alt="perrodonaciones"/>
+    <div >
+        <Row justify='space-around' align='middle' >
+            <Col><span><strong>
+                <img src={img1} alt="perrodonaciones"/>
+
+            </strong></span>
             </Col>
-            <Col span={8}>
-                    <h1>DONACIONES</h1>
-                    <Link to={Routes.DONACIONESFISICAS}><Button type="primary" className='buttonoption'>Donaciones físicas</Button></Link>
-                    <Link to={Routes.DONACIONESTRANSFERENCIA}><Button type="primary" className='buttonoption'>Donaciones Transferencia bancaria</Button></Link>
-                    <Link to={Routes.DONACIONESDEBITO}><Button type="primary" className='buttonoption' >Donaciones Débito Bancario</Button></Link>
-                    <paragraph classname='paragraphfrase'>A veces sentimos que lo que hacemos es tan solo una gota en el mar, pero el mar seria menos si le faltara esa gota</paragraph>
-                    <paragraph>(Madre Teresa de Calcuta).</paragraph>
+
+            <Col ><span><strong>
+                 <h1>DONACIONES</h1>
+                    <Link to={Routes.DONACIONESFISICAS}><Button  className='buttonoption'>Donaciones físicas</Button></Link>
+                    <Link to={Routes.DONACIONESTRANSFERENCIA}><Button  className='buttonoption'>Donaciones Transferencia bancaria</Button></Link>
+                    <Link to={Routes.DONACIONESDEBITO}><Button className='buttonoption' >Donaciones Débito Bancario</Button></Link>
+            </strong></span>
             </Col>
-            <Col span={8}>
-                    <img src={img2} alt="gatodonaciones"/>
-            </Col>
+
         </Row>
+        <p className='paragraphfrase'>A veces sentimos que lo que hacemos es tan solo una gota en el mar, pero el mar seria menos si le faltara esa gota
+            (Madre Teresa de Calcuta).</p>
+
     </div>
 );
 
