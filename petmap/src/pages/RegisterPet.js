c478568979f1bf0d2f20014647ff4b32ba75317e
CarlosMontesdeoca <mateo.game@hotmail.com>
2020-09-22 11:01:41 -0500

    registro exitoso de animales con firebase

--- petmap/src/pages/RegisterPet.js
+++ petmap/src/pages/RegisterPet.js
@@ -1,63 +1,144 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../styles/RegisterPet.less'
-import { Form, Input, InputNumber, Button , Layout} from 'antd';
+import adopta from '../images/adopta.png'
+import { Form, Input, Select, Upload, Button , message} from "antd";
+import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
+import FIREBASE from "../firebase";
 
-const layout = {
-    labelCol: { span: 8 },
-    wrapperCol: { span: 16 },
-};
-
-const validateMessages = {
-    required: '${label} is required!',
-    types: {
-        email: '${label} is not validate email!',
-        number: '${label} is not a validate number!',
-    },
-    number: {
-        range: '${label} must be between ${min} and ${max}',
-    },
-};
 
 const RegisterPet = () => {
 
-    const onFinish = values => {
-        console.log(values);
+    const normFile = e => {
+        console.log('Upload event:', e);
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e && e.fileList;
     };
 
-    return (
+    const handleSubmit = async (values) =>{
+        console.log('form', values);
+
+        const image = values.foto[0].originFileObj;
+
+        const uploadSnapshot = await FIREBASE.storage.ref('animals/' + values.foto[0].name + '_ ' + Date.now()).put(image);
+        const imageURL = await uploadSnapshot.ref.getDownloadURL();
+        console.log(imageURL);
+
+
+        await FIREBASE.db.ref('AdoptionPet').push({
+            ...values,
+            foto: imageURL
+        });
+        message.success('Los datos se an registrado correctamente');
+    }
+
+
+    return(
         <>
-        <div className="main-reg">
-            <div className=" title-reg"><h3> AYUDA A UN NUEVO AMIGO A CONSEGUIR UN HOGAR </h3></div>
-            <div className="row">
-                <div className="col-5 photo">
-                    <Button type="btn btn-access">Cargar foto</Button>
-                </div>
-                <div className="col-7 form-reg">
-                    <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-                        <Form.Item name={['user', 'tipo']} label="Tipo: " rules={[{ required: true }]}>
-                            <Input />
-                        </Form.Item>
-                        <Form.Item name={['user', 'estado']} label="Estado: " rules={[{ required: true }]}>
-                            <Input />
-                        </Form.Item>
-                        <Form.Item name={['user', 'direccion']} label="Direccion: " rules={[{ required: true }]}>
-                            <Input />
-                        </Form.Item>
-                        <Form.Item name={['user', 'descripcion']} label="Descripcion: " rules={[{ required: true }]}>
-                            <Input.TextArea />
-                        </Form.Item>
-                        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-                            <Button type="btn btn-access" htmlType="submit">
-                                Registrar
-                            </Button>
-                        </Form.Item>
-                    </Form>
+            <div className="main-reg">
+                <div className=" title-reg"><h3> AYUDA A UN NUEVO AMIGO A CONSEGUIR UN HOGAR </h3></div>
+                <div className="row">
+                    <div className="col-5 photo">
+                        <img src={adopta} alt="logo"/>
+                    </div>
+                    <div className="col-7 form-reg">
+                        <Form
+                            name='pet-form'
+                            onFinish={handleSubmit}
+                        >
+                            <Form.Item label="Foto">
+                                <Form.Item name="foto" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+                                    <Upload.Dragger name="files" action="/upload.do">
+                                        <p className="ant-upload-drag-icon">
+                                            <InboxOutlined />
+                                        </p>
+                                        <p className="ant-upload-text">Click para subir la foto del animalito</p>
+                                    </Upload.Dragger>
+                                </Form.Item>
+                            </Form.Item>
+                            <Form.Item
+                                label='Tipo de animal"'
+                                name='tipo'
+                                rules={ [
+                                    {
+                                        required: true,
+                                        message: 'este campo es requerido'
+                                    }
+                                ]}
+                            >
+                                <Select>
+                                    <Select.Option value="perro">Perro</Select.Option>
+                                    <Select.Option value="gato">Gato</Select.Option>
+                                </Select>
+                            </Form.Item>
+                            <Form.Item
+                                label='Estado'
+                                name='estado'
+                                rules={ [
+                                    {
+                                        required: true,
+                                        message: 'este campo es requerido'
+                                    }
+                                ]}
+                            >
+                                <Select>
+                                    <Select.Option value="enfermo">Enfermo</Select.Option>
+                                    <Select.Option value="saludable">Saludable</Select.Option>
+                                    <Select.Option value="desnutrido">Desnutrido</Select.Option>
+                                    <Select.Option value="grave">Grave</Select.Option>
+
+                                </Select>
+                            </Form.Item>
+                            <Form.Item
+                                label='Barrio: '
+                                name='barrio'
+                                rules={ [
+                                    {
+                                        required: true,
+                                        message: 'este campo es requerido'
+                                    }
+                                ]}
+                            >
+                                <Input placeholder="en que barrio se encontro al animalito"/>
+                            </Form.Item>
+                            <Form.Item
+                                label='Calle: '
+                                name='calle'
+                                rules={ [
+                                    {
+                                        required: true,
+                                        message: 'este campo es requerido'
+                                    }
+                                ]}
+                            >
+                                <Input placeholder="en que calle se encontro al animalito"/>
+                            </Form.Item>
+                            <Form.Item
+                                label='Descripción: '
+                                name='descripcion'
+                                rules={ [
+                                    {
+                                        required: true,
+                                        message: 'este campo es requerido'
+                                    }
+                                ]}
+                            >
+                                <Input.TextArea placeholder="describe el aspecto del animalito"/>
+                            </Form.Item>
+
+                            <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
+                                <Button className="submit" type="primary" htmlType="submit">
+                                    Registrar
+                                </Button>
+                            </Form.Item>
+                        </Form>
+
+                    </div>
                 </div>
             </div>
-        </div>
-
         </>
-    );
+    )
 };
 
 export default RegisterPet;
